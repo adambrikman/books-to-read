618d02ea9f0d1c56a779d3bf8cda9e22ca560920
Adam <adambrikman@gmail.com>
2019-09-11 22:24:07 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Define routes
+
+// Middleware
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
+app.use = cors();
+
+// Connect to Mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Utilize routes
+
+const db = mongoose.connection;
+db.on("error", err => console.error(err));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.listen(process.env.port || 3000);
