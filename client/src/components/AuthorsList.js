d16b6c0d84f5a45193ade2af0be8c9840c615d75
Adam <adambrikman@gmail.com>
2019-09-11 22:55:36 -0700

    rearrange file structure for heroku

---
+++ client/src/components/AuthorsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AuthorsList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>AuthorsList</h1>
+      </div>
+    );
+  }
+}
+
+export default AuthorsList;
