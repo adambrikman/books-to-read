898b05f6a692f04f8119869d8b1cdce5ddda75be
Adam <adambrikman@gmail.com>
2019-09-24 07:59:35 -0700

    update server.js with updated path for heroku

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
   });
 }
 
