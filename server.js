76627328ffec37f218b1e50375a9002be49423c8
Adam <adambrikman@gmail.com>
2019-09-24 08:28:25 -0700

    updated path

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 
-app.use(express.static("./client/dist"));
+app.use(express.static(path.join(__dirname + "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+  res.sendFile(path.join(__dirname + "/client/dist/" + "index.html"));
 });
 
 app.listen(process.env.PORT || 3000);
