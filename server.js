34361a32f309bd0c2bfde1b0a132b3d09ec405bd
Adam <adambrikman@gmail.com>
2019-09-24 16:06:33 -0700

    update package.json and server.js

--- server.js
+++ server.js
@@ -6,19 +6,25 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 
 // Middleware
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
+app.use(morgan("dev"));
 
+// process.env.MONGO_URI
 // Connect to Mongoose
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://user:LTLCSYSIOmetHfxP@bookstoread-y0tzd.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", err => console.error(err));
@@ -37,7 +43,7 @@ app.use("/books", bookRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
   });
 }
 
